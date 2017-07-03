cc7fd4d951ddeb05fffe3dea2145eda0d09c8349
Joe Wass <jwass@crossref.org>
2017-07-03 12:17:04 +0100

    Remove resources from Dockerfile.

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,6 @@ MAINTAINER Joe Wass jwass@crossref.org
 
 COPY src /usr/src/app/src
 COPY test /usr/src/app/test
-COPY resources /usr/src/app/resources
 COPY project.clj /usr/src/app/project.clj
 
 WORKDIR /usr/src/app
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject event-data-reddit-links-agent "0.2.0"
+(defproject event-data-reddit-links-agent "0.2.1"
   :description "Crossref Event Data Reddit.com Agent"
   :url "http://eventdata.crossref.org"
   :license {:name "The MIT License (MIT)"
